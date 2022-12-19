65aa6421fed3ccdc5a826d5d894c3f7c6f68caa8
Amay Kataria <amay.kataria@gmail.com>
2022-12-19 18:39:49 +0100

    Updates

---
+++ precache-manifest.4c573392824173f374b20287c111bd3a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee05db196ce07294e2c4665cee4039c",
+    "url": "/Liarbirds/index.html"
+  },
+  {
+    "revision": "44ea2302ac7d3e5fb34d",
+    "url": "/Liarbirds/static/css/main.dbfc8850.chunk.css"
+  },
+  {
+    "revision": "742873e3f51994e3bb30",
+    "url": "/Liarbirds/static/js/2.5421ec5f.chunk.js"
+  },
+  {
+    "revision": "e1e3d6106dc79d4567113c24c9b993d8",
+    "url": "/Liarbirds/static/js/2.5421ec5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44ea2302ac7d3e5fb34d",
+    "url": "/Liarbirds/static/js/main.7f8ececc.chunk.js"
+  },
+  {
+    "revision": "9688d6b1c867531c03bf",
+    "url": "/Liarbirds/static/js/runtime-main.b26479b8.js"
+  },
+  {
+    "revision": "e13ce42a9d64e753315088909c4bed53",
+    "url": "/Liarbirds/static/media/demode-webfont.e13ce42a.woff"
+  },
+  {
+    "revision": "f4adb378be7be27778730bb821970f70",
+    "url": "/Liarbirds/static/media/demode-webfont.f4adb378.woff2"
+  },
+  {
+    "revision": "6acdacdc31196f4007a5cc48e1340130",
+    "url": "/Liarbirds/static/media/din-webfont.6acdacdc.woff2"
+  },
+  {
+    "revision": "e6a361ef75e47721d5e73bbb557d7635",
+    "url": "/Liarbirds/static/media/din-webfont.e6a361ef.woff"
+  },
+  {
+    "revision": "2814188bf64d07b3583abf175bf443b8",
+    "url": "/Liarbirds/static/media/front_page.2814188b.jpg"
+  },
+  {
+    "revision": "6049994a377c286ba27e01c2fb4685eb",
+    "url": "/Liarbirds/static/media/hyde_park.6049994a.jpg"
+  },
+  {
+    "revision": "5ff4e0544347d214449dfc2f1989c979",
+    "url": "/Liarbirds/static/media/jellyman.5ff4e054.glb"
+  },
+  {
+    "revision": "87b33e2de3819ca5260319ef546738c9",
+    "url": "/Liarbirds/static/media/opensans-light-webfont.87b33e2d.woff2"
+  },
+  {
+    "revision": "9ad03de81bbf6cd48ffc44cf2aa4d707",
+    "url": "/Liarbirds/static/media/opensans-light-webfont.9ad03de8.woff"
+  },
+  {
+    "revision": "65a61f9230a85b44df43968f80a522e5",
+    "url": "/Liarbirds/static/media/opensans-regular-webfont.65a61f92.woff"
+  },
+  {
+    "revision": "d407a58546593548175615d7621183ef",
+    "url": "/Liarbirds/static/media/opensans-regular-webfont.d407a585.woff2"
+  },
+  {
+    "revision": "d5d726e759f12190627dc939df784662",
+    "url": "/Liarbirds/static/media/postcard_back.d5d726e7.jpg"
+  },
+  {
+    "revision": "37e7188300a1b8c7ee0b3074db77d86f",
+    "url": "/Liarbirds/static/media/slice-webfont.37e71883.woff2"
+  },
+  {
+    "revision": "60013d1c9c4b0368a971b7814f6b98f3",
+    "url": "/Liarbirds/static/media/slice-webfont.60013d1c.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d50d3fb5333fc5fae6162cb549a417d5.js"
+  "/Liarbirds/precache-manifest.4c573392824173f374b20287c111bd3a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Liarbirds/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
